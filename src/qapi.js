15e7364e4db4946b379fe651483a70fc39efbd57
Tanmaishah <65083113+Tanmaishah@users.noreply.github.com>
2020-12-08 06:24:59 -0500

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fcf61c7e563f3001ecb824d",
+				"5fcf61c7e563f3001ecb824f"
+			]
+		},
+		"5fcf61c7e563f3001ecb824d": {
+			"id": "5fcf61c7e563f3001ecb824d",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fcf61c7e563f3001ecb824f": {
+			"id": "5fcf61c7e563f3001ecb824f",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
